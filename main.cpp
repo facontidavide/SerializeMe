565be5a92c671f14849d4811ad06e7b4881c2422
Davide Faconti <davide.faconti@gmail.com>
2018-10-18 21:14:32 +0200

    comments

--- main.cpp
+++ main.cpp
@@ -10,21 +10,34 @@ int main()
 {
     uint8_t array[8];
     ByteSpan buffer(array);
-    cout << buffer.size() << endl;
+    cout << "buffer size: " << buffer.size() << endl;
 
+    // Serialize into the buffer
 
     ByteSpan buffer2 = buffer;
-    buffer2 = SerializeIntoBuffer( float(33.7), buffer2 );
-    buffer2 = SerializeIntoBuffer( int32_t(-42), buffer2 );
-    cout << buffer2.size() << endl;
+    cout << "\nbuffer2 size: " <<  buffer2.size() << endl;
 
-    ByteSpan buffer3 = buffer;
+    buffer2 = SerializeIntoBuffer( float(33.7), buffer2 );
+    cout << "buffer2 size: " <<  buffer2.size() << endl;
+
+    buffer2 = SerializeIntoBuffer( int32_t(-42), buffer2 );
+    cout << "buffer2 size: " <<  buffer2.size() << endl;
+
+    // Deserialize from the buffer
+
     float num_f = 0;
     int32_t num_i = 0;
+
+    ByteSpan buffer3 = buffer;
+    cout << "\nbuffer3 size: " <<  buffer3.size() << endl;
+
     buffer3 = DeserializeFromBuffer( buffer3, num_f );
+    cout << "buffer3 size: " <<  buffer3.size() << endl;
+
     buffer3 = DeserializeFromBuffer( buffer3, num_i );
-    cout << buffer3.size() << endl;
-    cout << num_f << " / " << num_i << endl;
+    cout << "buffer3 size: " <<  buffer3.size() << endl;
+
+    cout << "\n" << num_f << " / " << num_i << endl;
 
     return 0;
 }
